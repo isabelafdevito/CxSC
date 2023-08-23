ef193cb88703902e42ce4f91d518242b53ad5a2c
Isabela de Vito <80789978+isabelafdevito@users.noreply.github.com>
2023-08-23 13:54:37 -0300

    Update spoofing.py

--- spoofing.py
+++ spoofing.py
@@ -1,3 +1,5 @@
+#scapy é uma ferramenta para enviar diversos tipos de pacotes do protocolo IP, podendo ser usado para manipular os pacotes de forma a enviar o endereço de origem IP falsificado para uma aplicação. 
+
 from scapy.all import *
 
 # Endereço IP falso de origem
